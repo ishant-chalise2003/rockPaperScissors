244e2751a9b9346c80f8cd82747f3c484fba5ce3
Ishant Chalise <ishantchalise2003@gmail.com>
2024-12-27 21:32:17 +0545

    Rock paper scissors using basic javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+compScore = 0
+playerScore = 0
+
+noOfRounds = prompt('How many round do you want to play?')
+
+function userEntry(){ // Asking for user choice
+    return (prompt('Press \n "1" for Rock\n "2" for Paper \n "3" for Scissors'))
+}
+
+function computerEntry(){
+    return Math.floor((Math.random()*10)%3)
+
+}
+
+function gamePlay(){
+    if (userChoice == computerChoice){
+        alert("Its a draw.")
+    }
+    else if ((userChoice == 2 && computerChoice == 1) || (userChoice == 3 && computerChoice == 2) || (userChoice == 1 && computerChoice == 3)){
+        playerScore += 1
+        alert ("You Won this round!")
+    
+    }else{
+        compScore += 1
+        alert("You lose this round!")
+    }
+
+}
+
+function result (playerScore, compScore){
+    if (playerScore > compScore){
+        alert("Congratulations, You Won the Game!")
+    }else if(compScore > playerScore){
+        alert("Sorry, you lost the game!")
+    }else{
+        alert("Its a draw!")
+    }
+}
+
+
+
+for (i = 1; i <= noOfRounds; i++){    // From round 1 to n
+    userChoice = userEntry()
+    computerChoice = computerEntry()
+    gamePlay()
+    
+    
+}
+
+
+result(playerScore, compScore)
